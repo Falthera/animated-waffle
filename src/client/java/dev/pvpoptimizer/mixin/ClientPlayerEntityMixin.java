6c52378bef92851593715d5289f1bd576d716465
Falthera <aaryadevv8@gmail.com>
2026-06-01 20:31:51 +1000

    Update ClientPlayerEntityMixin.java

--- src/client/java/dev/pvpoptimizer/mixin/ClientPlayerEntityMixin.java
+++ src/client/java/dev/pvpoptimizer/mixin/ClientPlayerEntityMixin.java
@@ -2,16 +2,19 @@ package dev.pvpoptimizer.mixin;
 
 import dev.pvpoptimizer.PvpOptimizerClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ClientPlayerEntity.class)
+@Mixin(LivingEntity.class)
 public abstract class ClientPlayerEntityMixin {
 	@Inject(method = "damage(Lnet/minecraft/class_1282;F)Z", at = @At("HEAD"))
 	private void pvpoptimizer$onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-		PvpOptimizerClient.controller().onLocalPlayerDamaged((ClientPlayerEntity) (Object) this, source, amount);
+		if ((Object) this instanceof ClientPlayerEntity player) {
+			PvpOptimizerClient.controller().onLocalPlayerDamaged(player, source, amount);
+		}
 	}
 }
